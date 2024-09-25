57d3d8951790abfda13b9ce9169848c99c27e9f4
Lwzkervin <990764375@qq.com>
2024-09-25 22:49:26 +0800

    element-ui导入/测试

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{msg}}
+ 
+    <!--图标-->
+    <div>
+      <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+      </el-row>
+    </div>
+ 
+    <!-- 日期选择器 -->
+    <DatePicker></DatePicker>
+    <!-- 文件上传 -->
+    <UploadFile></UploadFile>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  // 导入组件
+  import DatePicker from './components/DatePicker.vue'
+  import UploadFile from './components/UploadFile.vue'
+ 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      msg: '测试msg'
+    }
+  },
+  components:{
+    DatePicker,
+    UploadFile
   }
 }
 </script>
-
+ 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ 
+h1, h2 {
+  font-weight: normal;
+}
+ 
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+ 
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+ 
+a {
+  color: #42b983;
 }
 </style>
