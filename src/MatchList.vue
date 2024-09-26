5a7398e83e5843103de36a82c234fcc06b317948
Lwzkervin <990764375@qq.com>
2024-09-26 16:53:36 +0800

    修复Bug

---
+++ src/MatchList.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="backgrouond">
+    <div>
+      <DatePicker @date-selected="handleDateSelected" />
+    </div>
+    <!-- 使用 MatchTableItem 组件并传递 props -->
+
+    <div class="container">
+      <MatchTableItem v-for="(item, index) in items" :key="index" :time="item.startDate" :event="item.event" :disciplineName="item.disciplineName"      
+        :team1="{
+        name: item.competitors[0].name,
+        flag: item.competitors[0].noc,
+        score: item.competitors[0].mark
+        }"
+        :team2="{
+        name: item.competitors[1].name,
+        flag: item.competitors[1].noc,
+        score: item.competitors[1].mark
+        }"
+      >
+      </MatchTableItem>
+    </div>
+  </div>
+</template>
+
+<script>
+import MatchTableItem from './components/MatchTableItem.vue';
+import DatePicker from './components/DatePicker.vue';
+import axios from 'axios';
+
+export default {
+  components: {
+    MatchTableItem,
+    DatePicker
+  },
+  data() {
+    return {
+      selectedDate: '0725',
+      items: []
+    };
+  },
+  mounted() {
+    this.fetchMatchList();
+  },
+  methods: {
+    handleDateSelected(date) {
+      const formattedDate = date.replace(/月|日/g, '').padStart(4, '0');
+      this.selectedDate = formattedDate;
+    },
+    async fetchMatchList() {
+      try {
+        const response = await axios.get(`./json/${this.selectedDate}.json`);
+        this.items = response.data;
+      } catch (error) {
+        console.error('Error fetching match list:', error);
+      }
+    }
+  },
+  watch: {
+    selectedDate() {
+      this.fetchMatchList();
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 14px;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+.backgrouond{
+  background-image: url('@/assets/background/football.jpeg'); /* 设置背景图片 */
+  background-size: cover; /* 背景图片覆盖整个容器 */
+  background-position: center; /* 背景图片居中 */
+  background-attachment: fixed; /* 背景图片固定 */
+
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui';                      // 引入element-ui
 import 'element-ui/lib/theme-chalk/index.css';           // element-ui的css样式要单独引入
-import App from './App.vue'
+// import App from './App.vue'
 // import MedalList from './MedalList.vue';
-// import MatchList from './MatchList.vue';
+import MatchList from './MatchList.vue';
  
 Vue.use(ElementUI);   // 这种方式引入了ElementUI中所有的组件
  
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(MatchList)
 })
